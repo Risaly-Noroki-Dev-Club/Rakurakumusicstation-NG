e382d596e56be684d4e3cf2148e78cf8b858efcc
HayabusaErika <ericchou2021@gmail.com>
2026-05-10 22:02:27 +0800

    fix: configure Vuetify 3 MDI iconset to fix emoji fallback icons

--- radio-backend/frontend/src/main.ts
+++ radio-backend/frontend/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
@@ -12,6 +13,13 @@ import './style.css'
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
   theme: {
     defaultTheme: 'light',
     themes: {
